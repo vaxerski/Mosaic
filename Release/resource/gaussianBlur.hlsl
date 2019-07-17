2eda5f2540b14de6325d6a3b0c2c5ca293d20cf1
vaxerski <ralfik.q@wp.pl>
2019-07-17 10:43:30 +0200

    Added the shader cuz i forgot

---
+++ Release/resource/gaussianBlur.hlsl
@@ -0,0 +1,32 @@
+uniform sampler2D texture;
+uniform vec2 blur_radius;
+uniform vec2 blur_radius2;
+
+void main() {
+    vec2 textureCoordinates = gl_TexCoord[0].xy;
+    vec4 color = vec4(0.0);
+    color += texture2D(texture, textureCoordinates - 10.0 * blur_radius) * 0.0012;
+    color += texture2D(texture, textureCoordinates - 9.0 * blur_radius) * 0.0015;
+    color += texture2D(texture, textureCoordinates - 8.0 * blur_radius) * 0.0038;
+    color += texture2D(texture, textureCoordinates - 7.0 * blur_radius) * 0.0087;
+    color += texture2D(texture, textureCoordinates - 6.0 * blur_radius) * 0.0180;
+    color += texture2D(texture, textureCoordinates - 5.0 * blur_radius) * 0.0332;
+    color += texture2D(texture, textureCoordinates - 4.0 * blur_radius) * 0.0547;
+    color += texture2D(texture, textureCoordinates - 3.0 * blur_radius) * 0.0807;
+    color += texture2D(texture, textureCoordinates - 2.0 * blur_radius) * 0.1065;
+    color += texture2D(texture, textureCoordinates - blur_radius) * 0.1258;
+    color += texture2D(texture, textureCoordinates) * 0.1330;
+    color += texture2D(texture, textureCoordinates + blur_radius2) * 0.1258;
+    color += texture2D(texture, textureCoordinates + 2.0 * blur_radius2) * 0.1065;
+    color += texture2D(texture, textureCoordinates + 3.0 * blur_radius2) * 0.0807;
+    color += texture2D(texture, textureCoordinates + 4.0 * blur_radius2) * 0.0547;
+    color += texture2D(texture, textureCoordinates + 5.0 * blur_radius2) * 0.0332;
+    color += texture2D(texture, textureCoordinates + 6.0 * blur_radius2) * 0.0180;
+    color += texture2D(texture, textureCoordinates - 7.0 * blur_radius2) * 0.0087;
+    color += texture2D(texture, textureCoordinates - 8.0 * blur_radius2) * 0.0038;
+    color += texture2D(texture, textureCoordinates - 9.0 * blur_radius2) * 0.0015;
+    color += texture2D(texture, textureCoordinates - 10.0 * blur_radius2) * 0.0012;
+    gl_FragColor = color;
+}
+
+//had to fix this internet c+p lol ik it's shit but who cares i'll make one in the future
